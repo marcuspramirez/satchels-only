44257c08be66f0ea096e55c37628f552dc7bb267
marcuspramirez <@macusramirez@gmail.com>
2022-10-21 14:59:47 -0500

    firebase should work

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProfileCardComponent } from './profile-card/profile-card.component';
 
-const routes: Routes = [
-  { path: 'profile-card', component: ProfileCardComponent },
-];
+const routes: Routes = [{ path: '', component: ProfileCardComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
